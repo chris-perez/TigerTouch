646ee0ef78360700534a1f463e5842609683f51c
cperez3 <chrisv.perez@gmail.com>
2017-04-05 23:10:51 -0400

    created function for getting the average value from around a mouse location

--- src/tigertouch/Main.java
+++ src/tigertouch/Main.java
@@ -1,6 +1,8 @@
 package tigertouch;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import static tigertouch.Animal.Category.AMPHIBIANS;
 
 public class Main {
@@ -20,7 +22,21 @@ public class Main {
     }
   }
 
+  public static double getTouchValue(int x, int y) {
+    int radius = 4;
+    double[][] grid = new double[100][100];
 
+    double sum = 0;
+    int numValues = 0;
+    for (int i = x - radius; i < x + radius; i++) {
+      for (int j = y - (radius - Math.abs((x - i))); j < y + (radius - Math.abs((x - i))); j++) {
+        sum += grid[x][y];
+        numValues++;
+      }
+    }
+
+    return sum/numValues;
+  }
 
   public static void selectAnimal(int i) {
     currentAnimal = animals.get(i);
